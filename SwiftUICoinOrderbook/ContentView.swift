58476a8ea120ab7fdf0c4c029c42158cd3b686d9
Hyuk0510 <hyouk970510@gmail.com>
2023-11-20 11:46:19 +0900

    Initial Commit

---
+++ SwiftUICoinOrderbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUICoinOrderbook
+//
+//  Created by 선상혁 on 2023/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICoinOrderbook/SwiftUICoinOrderbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinOrderbookApp.swift
+//  SwiftUICoinOrderbook
+//
+//  Created by 선상혁 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinOrderbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
